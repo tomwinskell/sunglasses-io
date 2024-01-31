d79bc5ac4a3283c0b954d2ce9a6709cc49a2c5eb
Brian Jenney <brianjenney83@gmail.com>
2024-01-31 15:13:02 -0800

    use const

--- app/server.js
+++ app/server.js
@@ -1,13 +1,11 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const http = require('http');
+const fs = require('fs');
+const finalHandler = require('finalhandler');
+const queryString = require('querystring');
+const Router = require('router');
+const bodyParser = require('body-parser');
+const uid = require('rand-token').uid;
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
-
-}).listen(PORT);
+http.createServer(function (request, response) {}).listen(PORT);
